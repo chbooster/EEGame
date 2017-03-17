5adcf1374d58b37305bf759019929c8d7efc9d2d
CHBOOSTER-MSI\chiri <carlos.aniortellanes@gmail.com>
2017-03-17 11:26:24 +0100

    Initial commit for sync starting off Visual Studio C++ project

---
+++ EEGame/EEGame/main.cpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////
+// EEGame TFG Research Project for Emotiv Insight - 2017 //
+// Carlos Aniorte Llanes                                 //
+// carlos.aniortellanes@gmail.com                        //
+///////////////////////////////////////////////////////////
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+	system("pause");
+
+	return 0;
+}
